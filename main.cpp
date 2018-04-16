6e976e9167b82c22a6ac5f800a88f6394404bdb4
Luis Alberto García Rodríguez <luis.gcdz@gmail.com>
2018-04-15 20:09:47 -0500

    Primer script para verificar la edad y confirmar si se puede votar

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+Programa_5 votaciones
+Estructura: selectiva simple
+autor: Luis Alberto García Rodríguez
+N° lista: 11
+Fecha 10 de febrero del 2015
+*/
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int edad;
+    cout << "**** Programa votaciones ****" << endl;
+    cout<<"\n\n Cual es tu edad: ";
+    cin>>edad;
+        if (edad>=18)
+        {
+            cout<<"\n\n Usted si puede votar"<<endl<<endl;
+        }
+    system ("pause");
+    return 0;
+}
